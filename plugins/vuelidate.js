0d450b80784878eacd1f434d4ea97816bad363bd
Marvin <Marvinofeld1991@web.de>
2019-12-01 17:24:54 +0100

    Added error validation

--- plugins/vuelidate.js
+++ plugins/vuelidate.js
@@ -1,3 +1,19 @@
 import vuelidate from "vuelidate";
 import Vue from "vue";
+import vuelidateErrorExtractor, { templates } from "vuelidate-error-extractor";
+npm
 Vue.use(vuelidate);
+
+Vue.use(vuelidateErrorExtractor, {
+  /**
+   * Optionally provide the template in the configuration.
+   * or register like so Vue.component("FormField", templates.singleErrorExtractor.foundation6)
+   */
+  template: templates.singleErrorExtractor.foundation6, // you can also pass your own custom template
+  messages: { required: "The {attribute} field is required" }, // error messages to use
+  attributes: { // maps form keys to actual field names
+    email: "Email",
+    first_name: "First name",
+    last_name: "Last name"
+  }
+});
