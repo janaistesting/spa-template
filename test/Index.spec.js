df9eae1e71f25c92abb1c1578581ca3f6d976ebf
Marvin <Marvinofeld1991@web.de>
2019-12-09 18:18:43 +0100

    Added footer tests

--- test/Index.spec.js
+++ test/Index.spec.js
@@ -1,9 +1,9 @@
-import {mount} from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import Footer from '@/components/footer/Footer.vue'
 
 describe('Footer', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Footer);
+    const wrapper = shallowMount(Footer);
     expect(wrapper.isVueInstance()).toBeTruthy();
   })
 });
