f38e46ab5f09f665f39a3bd2e116069ee6c47755
Marvin <Marvinofeld1991@web.de>
2019-12-17 19:57:52 +0100

    adjusted pipeline

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   }
 } : {};
 
-const routes: [
+const routes = [
   'references/reference/floriandanger',
   'references/reference/juleslamy',
   'references/reference/nicolelorenzmattke',
