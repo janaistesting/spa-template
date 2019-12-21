2f13dd067953ce76f35d884454299a1f71d1e8e3
Marvin <Marvinofeld1991@web.de>
2019-12-21 12:30:14 +0100

    Added ci cd for lightsail

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+remote=18.184.46.63
+user=bitnami
+sshkey=~/.ssh/scp.pem
+
+#Get Node version
+echo Node Version: $(node --version)
+
+# install nuxt to generate static content
+npm install -g nuxt
+
+npm run --prefix /app/ generate
+
+echo Hello
+
+
+#For gitbash
+#scp -i $sshkey -r ../dist/ $user@$remote:/home/bitnami/stack/nginx/html/
