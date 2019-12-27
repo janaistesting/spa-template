b1f4d4551097874a1f5f0a3cccea4203e250555b
janaistesting <janaistesting@gmail.com>
2019-12-27 13:01:53 +0100

    First Steps Yana

---
+++ app/pages/tutorialY/index.vue
@@ -0,0 +1,16 @@
+<template src="./index.pug" lang="pug"></template>
+
+<script>
+    import Page from "@/components/page/Page.vue";
+
+    export default {
+        name: "index",
+        components: {
+            Page
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
